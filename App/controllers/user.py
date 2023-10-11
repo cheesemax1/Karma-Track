c7ef296edb7f7a92d005d03c1008176d0f5349d1
Jarrod Moore <jarrod.moore@my.uwi.edu>
2023-10-11 22:59:03 +0000

    updating user

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,11 +1,19 @@
 from App.models import User
 from App.database import db
 
-def create_user(username, password):
-    newuser = User(username=username, password=password)
+def create_user(name, username, password, user_type):
+  try:
+    newuser = User(name=name,
+                   username=username,
+                   password=password,
+                   user_type=user_type)
     db.session.add(newuser)
     db.session.commit()
     return newuser
+  except Exception as e:
+    print('error in creating user: ', e)
+    db.session.rollback()
+    return None
 
 def get_user_by_username(username):
     return User.query.filter_by(username=username).first()
@@ -30,4 +38,9 @@ def update_user(id, username):
         db.session.add(user)
         return db.session.commit()
     return None
-    
+
+def is_admin(id):
+  user = get_user(id)
+  if user:
+    return user.user_type == 'Admin'
+  return Fales
