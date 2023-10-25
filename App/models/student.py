0dc746050b4cd295588724bd2c625028336d9286
Matthew Roodal <matthewjoshroodal@gmail.com>
2023-10-25 19:01:49 +0000

    Updated Student Model

--- App/models/student.py
+++ App/models/student.py
@@ -1,22 +1,15 @@
 from App.database import db
 from .person import Person
 from .student_course import student_course
+from datetime import date
 
 
 class Student(Person):
   __tablename__ = 'student'
-  year = db.Column(db.Date, nullable=False)
+  year = db.Column(db.Integer, nullable=False)
   karma = db.Column(db.Integer, default=0)
-  studentreviews = db.relationship('Review',
-                                   backref=db.backref('student'),
-                                   lazy='joined')
-  courses = db.relationship('Course',
-                            secondary=student_course,
-                            backref='enrolled_students')
-
-
-def __repr__(self):
-  return f'<Student :{self.name}, Year :{self.year}, Karma: {self.karma}>'
+  reviews = db.relationship('Review', backref=db..backref('student'), lazy = 'joined')
+  courses = db.relationship('Course',secondary=student_course,backref='enrolled_students')
 
 
 def __init__(self, name, year):
@@ -24,11 +17,16 @@ def __init__(self, name, year):
   self.year = year
   self.karma = 0
 
+def __repr__(self):
+  return f'<Student :{self.name}, Year :{self.year}, Karma: {self.karma}>'
+
 
 def toJSON(self):
   return {
-      'id': self.id,
-      'name': self.name,
-      'year': self.year,
-      'karma': self.karma
+      "id": self.id,
+      "name": self.name,
+      "year": self.year,
+      "karma": self.karma,
+      "courses" :courses,
+      "reviews": reviews
   }
